45f8333d3cbd060a1c40611049b2bd21e7f7dab1
Mono <ferch5003@gmail.com>
2024-06-24 02:41:01 -0500

    feat: Add Bot Repository

---
+++ user-service/internal/bot/repository.go
@@ -0,0 +1,35 @@
+package user
+
+import (
+	"context"
+	"github.com/jmoiron/sqlx"
+	"user-service/internal/domain"
+)
+
+// Queries.
+const (
+	_getBotByNameStmt = `SELECT id, name FROM bots WHERE name = ?;`
+)
+
+type Repository interface {
+	// Get obtain one Bot by the name.
+	Get(ctx context.Context, name string) (domain.Bot, error)
+}
+
+type repository struct {
+	conn *sqlx.DB
+}
+
+func NewRepository(conn *sqlx.DB) Repository {
+	return &repository{conn: conn}
+}
+
+func (r *repository) Get(ctx context.Context, name string) (domain.Bot, error) {
+	var bot domain.Bot
+
+	if err := r.conn.GetContext(ctx, &bot, _getBotByNameStmt, name); err != nil {
+		return domain.Bot{}, err
+	}
+
+	return bot, nil
+}
---
+++ user-service/internal/bot/repository_test.go
@@ -0,0 +1,78 @@
+package user
+
+import (
+	"context"
+	"errors"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/require"
+	"regexp"
+	"testing"
+	"user-service/internal/domain"
+)
+
+func TestRepositoryGet_Successful(t *testing.T) {
+	// Given
+	db, mock, err := sqlmock.New()
+	require.NoError(t, err)
+
+	defer db.Close()
+
+	dbx := sqlx.NewDb(db, "sqlmock")
+
+	ctx := context.Background()
+
+	expectedBotName := "Test"
+	expectedBot := domain.Bot{
+		ID:   1,
+		Name: "Test",
+	}
+
+	columns := []string{"id", "name"}
+	rows := sqlmock.NewRows(columns)
+	rows.AddRow(expectedBot.ID, expectedBot.Name)
+	mock.ExpectQuery("SELECT .*").WillReturnRows(rows)
+
+	repository := NewRepository(dbx)
+
+	// When
+	bot, err := repository.Get(ctx, expectedBotName)
+
+	// Then
+	require.NoError(t, err)
+	require.NotNil(t, bot)
+	require.Equal(t, expectedBot, bot)
+	require.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestRepositoryGet_FailsDueToInvalidGet(t *testing.T) {
+	// Given
+	db, mock, err := sqlmock.New()
+	require.NoError(t, err)
+
+	defer db.Close()
+
+	dbx := sqlx.NewDb(db, "sqlmock")
+
+	ctx := context.Background()
+
+	wrongQuery := regexp.QuoteMeta("SELECT wrong FROM bots;")
+	expectedError := errors.New(`Query: could not match actual sql: \"SELECT id, name 
+										FROM users WHERE name = ?;\" with expected regexp \"SELECT 
+										wrong FROM bots;\"`)
+
+	expectedBot := domain.Bot{}
+
+	mock.ExpectQuery(wrongQuery).WillReturnError(expectedError)
+
+	repository := NewRepository(dbx)
+
+	// When
+	bot, err := repository.Get(ctx, "")
+
+	// Then
+	require.Equal(t, expectedBot, bot)
+	require.ErrorContains(t, err, "Query")
+	require.ErrorContains(t, err, "could not match actual sql")
+	require.ErrorContains(t, err, "with expected regexp")
+}
